6d44a5e2378570068afd5d5351413ca1510a0e99
Rey Perez <cuyareyperez12@gmail.com>
2025-12-05 05:29:18 +0800

    login success

--- routes/loggingroutes.js
+++ routes/loggingroutes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const { logUserLogin } = require('../controllers/loggingcontroller');
 
 router.post("/",logUserLogin);
-( async (req, res) => {
-    res.json({ message: "Login route working!" });
-});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 // ---------------------------
 // Middleware
 // ---------------------------
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
 app.use(express.json()) // parse JSON request bodies
 
 // ---------------------------
